3973792be0e3acad24bcf7ac53b196bdb1907ee9
Delmanat3 <88867653+Delmanat3@users.noreply.github.com>
2021-11-24 11:25:18 -0600

    netfly is meanie

--- my-portfolio/src/components/card.js
+++
@@ -1,8 +0,0 @@
-const state=[{
-
-    title:['project 1','project 2','project 3'],
-
-       images:["./images/imgs/hp3.png","./images/imgs/weather pic.png","./images/imgs/plannerpic.png","https://watch.screencastify.com/v/sZMKUUOIgyInmMp3qsqD"],
-       link:["https://cole-cochran.github.io/crypto-cosmos/","https://delmanat3.github.io/weather-page/","https://delmanat3.github.io/daily-planner/"],
-       content:[]
-   }]
