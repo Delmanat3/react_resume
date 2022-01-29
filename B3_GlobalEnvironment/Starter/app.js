bd4a76c4279d64381c9a92a22420e62b845e31c9
Delmanat3 <88867653+Delmanat3@users.noreply.github.com>
2022-01-28 20:20:42 -0600

    update

---
+++ B3_GlobalEnvironment/Starter/app.js
@@ -0,0 +1,4 @@
+const a ="hello world";
+function b(){
+     console.log('called b');
+}
---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const compression = require("compression");
+// const routes = require("./controllers/index");
+const { ApolloServer } = require('apollo-server-express');
+
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+//const logger = require("morgan");
+
+const path =require('path');
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+  });
+
+
+//* Get ApolloServer and grab typeDefs and resolvers from schemas
+
+//* grab express routes from the controller
+// const routes = require('./controllers/index');
+
+//* set up port andexpress app
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+//app.use(logger("dev"));
+
+server.applyMiddleware({ app });
+
+app.use(compression());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+app.use(express.static('public'));
+
+
+// if (process.env.NODE_ENV === 'production') {
+// 	app.use(express.static(path.join(__dirname, '../client/build')));
+//   }
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/public/index.html'));
+  });
+
+//* connect to mongodb database
+
+//* set up middleware for routes
+// app.use(routes);
+
+//* set up server to listen on port and open connection to graphql
+db.once('open', () => {
+	app.listen(PORT, () => {
+		console.log(`App running on port ${PORT}!`);
+		console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+	});
+});
